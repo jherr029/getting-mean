0bb27f7b90640a118d3a7092f2cfe126f236be83
jherr029 <jherr029@ucr.edu>
2018-04-07 21:53:08 -0700

    for victor

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main')
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
 var homepageController = function ( req, res) {
   res.render('index', { title: 'Express'});
 };
 
-/* GET home page */
-router.get('/', ctrlMain.index);
+
+/* Location pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 
 module.exports = router;
