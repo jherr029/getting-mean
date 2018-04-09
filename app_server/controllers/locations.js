2e1a23193c499f518ecfbe2328058cb9c8f07add
jherr029 <jherr029@ucr.edu>
2018-04-09 14:37:51 -0700

    changed data to the controller from view

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,32 @@
 /* GET 'home' page */
 module.exports.homelist = function(req, res){
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', { 
+        title: 'Loc8r - find a place to work with wifill',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near youa!'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when you are out and about.",
+        locations: [{
+            name: 'Starcups',
+            address: '125 high street, reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'food', 'premium wifi'],
+            distance: '100mm'
+        }, {
+            name: 'Cafe Hero',
+            address: '321 Make Believe Street, Imagination',
+            rating: 4,
+            facilities: ['Hot drinks', 'Cold drinks', 'Premium wifi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '312 Make Believe Street, Imagination',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '210m'
+        }]
+    });
 };
 
 /* GET 'Location info' page */
